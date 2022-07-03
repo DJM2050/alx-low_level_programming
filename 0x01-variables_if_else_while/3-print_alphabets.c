03ff2850659d2e23c8558d7524c5f8fe60b3ae62
DJM2050 <myderetest@gmail.com>
2022-07-03 08:45:18 -0700

    test

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,13 +8,16 @@
  */
 int main(void)
 {
-	char low;
-
-	for (low = 'a'; low <= 'z'; low++)
-		putchar(low);
-	for (low = 'A'; low <= 'Z'; low++)
-		putchar(low);
-		putchar('\n');
+	int ch;
+	for (ch = 97; ch  <= 122; ch++)
+	{
+		putchar(ch);
+	}
+	for ( ch = 65; ch <= 90; ch++)
+	{
+		putchar(ch);
+	}
+	putchar(10);
 
 	return (0);
 }
